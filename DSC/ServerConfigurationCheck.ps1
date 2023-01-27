012340b8842489d8dabb77a016a8212dbd4bb1f0
jaehwanjoa <90813478+jaehwanjoa@users.noreply.github.com>
2023-01-27 16:38:15 +0900

    Add files via upload

---
+++ DSC/ServerConfigurationCheck.ps1
@@ -0,0 +1,13 @@
+﻿configuration ServerConfigurationCheck
+{
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Node localhost
+    {
+        WindowsFeatureSet WindowsFeatureSetExample
+        {
+            Name                    = @("SMTP-Server", "Web-Server")
+            Ensure                  = 'Present'
+            IncludeAllSubFeature    = $true
+        }
+    }
+}
