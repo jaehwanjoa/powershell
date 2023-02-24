fbd1b26328361912f7b3b8e6ccb5123d8509af57
jaehwanjoa <90813478+jaehwanjoa@users.noreply.github.com>
2023-02-24 15:28:27 +0900

    Update GetWindowsEvent.ps1

--- Microsoft/GetWindowsEvent.ps1
+++ Microsoft/GetWindowsEvent.ps1
@@ -1,4 +1,14 @@
-function Get-SecurityEvent {
+function SecurityEvent {
+    <#
+    .synopsis
+    이 파일은 Windows 이벤트 로깅 펑션 파일입니다.
+    .description
+    1. 이벤트ID를 입력합니다. 이벤트ID는 'eventviewer'에서 조회할 수 있습니다. 2. Application or System or Security 로그를 선택합니다.
+    .parameter $ComputerName
+    $env:COMPUTERNAME은 로컬 컴퓨터 네임으로 자동 변환됩니다.
+    .example
+    'SecurityEvent'라고 커멘드를 입력하면 자동으로 실행됩니다. 
+    #>
     [CmdletBinding()]
     param (
         [ValidateNotNullOrEmpty()]
