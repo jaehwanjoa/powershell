6d948f55d104118031953a743da2e3c1dc2dab52
jaehwanjoa <90813478+jaehwanjoa@users.noreply.github.com>
2023-10-19 16:06:01 +0900

    Create mde_mssp_restapi.ps1

---
+++ mde_mssp_restapi.ps1
@@ -0,0 +1,59 @@
+	1. Powershell을 사용하여 액세스 토큰 가져오기
+	https://learn.microsoft.com/en-us/microsoft-365/security/defender-endpoint/exposed-apis-full-sample-powershell?view=o365-worldwide#get-token
+$tenantId = 'a74b5451-a280-4768-a5cd-cb47af153c71' # Paste your own tenant ID here
+$appId = '1a8207f1-89ed-4f12-a488-fc883df705e8' # Paste your own app ID here
+$appSecret = 'rkR8Q~~wepDy9Enz304Rn2JuQE1z4PETwXYiVaPw' # Paste your own app secret here
+$suspiciousUrl = 'www.suspiciousUrl.com' # Paste your own URL here
+
+$resourceAppIdUri = 'https://securitycenter.onmicrosoft.com/windowsatpservice'
+$oAuthUri = "https://login.microsoftonline.com/$TenantId/oauth2/token"
+$authBody = [Ordered] @{
+    resource = "$resourceAppIdUri"
+    client_id = "$appId"
+    client_secret = "$appSecret"
+    grant_type = 'client_credentials'
+}
+$authResponse = Invoke-RestMethod -Method Post -Uri $oAuthUri -Body $authBody -ErrorAction Stop
+$aadToken = $authResponse.access_token
+
+
+#Get latest alert
+$alertUrl = "https://api.securitycenter.microsoft.com/api/alerts?`$top=10"
+$headers = @{ 
+    'Content-Type' = 'application/json'
+    Accept = 'application/json'
+    Authorization = "Bearer $aadToken" 
+}
+$alertResponse = Invoke-WebRequest -Method Get -Uri $alertUrl -Headers $headers -ErrorAction Stop
+$alerts =  ($alertResponse | ConvertFrom-Json).value
+
+$machinesToInvestigate = New-Object System.Collections.ArrayList
+
+Foreach($alert in $alerts)
+{
+    #echo $alert.id $alert.machineId    $alert.severity $alert.status
+
+    $isSevereAlert = $alert.severity -in 'Medium', 'High'
+    $isOpenAlert = $alert.status -in 'InProgress', 'New'
+    if($isOpenAlert -and $isSevereAlert)
+    {
+        if (-not $machinesToInvestigate.Contains($alert.machineId))
+        {
+            $machinesToInvestigate.Add($alert.machineId) > $null
+        }
+    }
+}
+
+$commaSeparatedMachines = '"{0}"' -f ($machinesToInvestigate -join '","')
+
+$query = "DeviceNetworkEvents
+| where DeviceId in ($commaSeparatedMachines)
+| where RemoteUrl  == `"$suspiciousUrl`"
+| summarize ConnectionsCount = count() by DeviceId"
+
+$queryUrl = "https://api.securitycenter.microsoft.com/api/advancedqueries/run"
+
+$queryBody = ConvertTo-Json -InputObject @{ 'Query' = $query }
+$queryResponse = Invoke-WebRequest -Method Post -Uri $queryUrl -Headers $headers -Body $queryBody -ErrorAction Stop
+$response =  ($queryResponse | ConvertFrom-Json).Results
+$response
